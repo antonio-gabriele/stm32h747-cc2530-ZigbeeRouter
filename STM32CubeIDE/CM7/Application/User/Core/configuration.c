e439c060a42d60c71698bb60371c21d5cb406171
antonio_gabriele <antonio.gabriele@gmail.com>
2021-12-18 18:58:51 +0100

    no message

---
+++ STM32CubeIDE/CM7/Application/User/Core/configuration.c
@@ -0,0 +1,18 @@
+#include "configuration.h"
+
+#include <pb.h>
+
+#include <fatfs.h>
+
+my_Configuration sys_cfg;
+
+void cfgRead() {
+	//if (f_mount(&SDFatFS, SDPath, 0) != FR_OK) {
+		//return;
+	//}
+	//f_open
+}
+
+void cfgWrite() {
+
+}
