3f4c99832490feb8f894e2373ab7fd31c2d464e0
antonio_gabriele <antonio.gabriele@gmail.com>
2021-12-27 15:26:41 +0100

    no message

--- STM32CubeIDE/CM7/Application/User/Zigbee/stack/appTask.c
+++ STM32CubeIDE/CM7/Application/User/Zigbee/stack/appTask.c
@@ -27,6 +27,25 @@ extern QueueHandle_t xQueueBackendToView;
 //
 devStates_t devState = DEV_HOLD;
 ResetReqFormat_t const_hard_rst = { .Type = 0 };
+//init ZDO device state
+uint8_t gSrcEndPoint = 1;
+uint8_t gDstEndPoint = 1;
+
+typedef struct {
+	uint16_t ChildAddr;
+	uint8_t Type;
+
+} ChildNode_t;
+
+typedef struct {
+	uint16_t NodeAddr;
+	uint8_t Type;
+	uint8_t ChildCount;
+	ChildNode_t childs[MAX_CHILDREN];
+} Node_t;
+
+Node_t nodeList[MAX_NODE_LIST];
+uint8_t nodeCount = 0;
 //
 static int32_t startNetwork1(void);
 //ZDO Callbacks
@@ -49,29 +68,6 @@ static uint8_t setNVPanID(uint32_t panId);
 static uint8_t setNVDevType(uint8_t devType);
 static int32_t startNetwork(void);
 static int32_t registerAf(void);
-typedef enum {
-	ZB_DANFOSS_ALLY,
-} airmon_zigbee_device_t;
-
-typedef struct {
-	uint8_t is_initialized;
-	uint32_t last_poll_time;
-} zigbee_device_info_t;
-
-typedef struct {
-	char manufact_name[32];
-	char device_name[32];
-	airmon_zigbee_device_t type;
-	uint16_t adr_short;
-	uint64_t adr_ieee;
-
-	zigbee_device_info_t info; // volatile, should not be saved
-} zigbee_device_t;
-
-zigbee_device_t* zigbee_device_get_free_handle(void) {
-	static zigbee_device_t device = { 0 };
-	return &device;
-}
 
 extern QueueHandle_t xQueueViewToBackend;
 
@@ -95,8 +91,8 @@ NULL,       // MT_ZDO_NWK_ADDR_RSP
 		NULL,      // MT_ZDO_IEEE_ADDR_RSP
 		NULL,      // MT_ZDO_NODE_DESC_RSP
 		NULL,     // MT_ZDO_POWER_DESC_RSP
-		NULL,    // MT_ZDO_SIMPLE_DESC_RSP
-		NULL,      // MT_ZDO_ACTIVE_EP_RSP
+		mtZdoSimpleDescRspCb,    // MT_ZDO_SIMPLE_DESC_RSP
+		mtZdoActiveEpRspCb,      // MT_ZDO_ACTIVE_EP_RSP
 		NULL,     // MT_ZDO_MATCH_DESC_RSP
 		NULL,   // MT_ZDO_COMPLEX_DESC_RSP
 		NULL,      // MT_ZDO_USER_DESC_RSP
@@ -113,7 +109,7 @@ NULL,       // MT_ZDO_NWK_ADDR_RSP
 		NULL,     // MT_ZDO_MGMT_DIRECT_JOIN_RSP
 		NULL,     // MT_ZDO_MGMT_PERMIT_JOIN_RSP
 		mtZdoStateChangeIndCb,   // MT_ZDO_STATE_CHANGE_IND
-		NULL,   // MT_ZDO_END_DEVICE_ANNCE_IND
+		mtZdoEndDeviceAnnceIndCb,   // MT_ZDO_END_DEVICE_ANNCE_IND
 		NULL,        // MT_ZDO_SRC_RTG_IND
 		NULL,	 //MT_ZDO_BEACON_NOTIFY_IND
 		NULL,			 //MT_ZDO_JOIN_CNF
@@ -136,294 +132,7 @@ static uint8_t mtSysVersionCb(VersionSrspFormat_t *msg) {
 	return 0;
 }
 
-void zigbee_device_save(zigbee_device_t *dev) {
-
-}
-
-void task_maintain_devices(void *param) {
-
-	while (1) {
-
-	}
-}
-
-void task_register_device(void *param) {
-	// parse parameter
-	uint16_t *adr_p = (uint16_t*) param;
-	uint16_t adr = *adr_p;
-
-	// feedback
-	show("[0x%04x] -> Device init started", adr);
-
-	// get free device handle
-	zigbee_device_t *dev = zigbee_device_get_free_handle();
-
-	// sanity check
-	if (dev == NULL) {
-		// feedback
-		show("[0x%04x] -> No more free handles", adr);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// update device address
-	dev->adr_short = adr;
-
-	// check if the device already exists
-	if (!znp_if_dev_exists(dev->adr_short)) {
-		// feedback
-		show("[0x%04x] -> Device not registered", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// refresh device info
-	if (znp_cmd_dev_refresh_info(dev->adr_short) != 0) {
-		// feedback
-		show("[0x%04x] -> Device couldn't be refreshed", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// register device info
-	if (znp_cmd_dev_register(dev->adr_short) != 0) {
-		// feedback
-		show("[0x%04x] -> Device couldn't be registered", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// read ieee address
-	if (znp_cmd_dev_get_ieee(dev->adr_short, &dev->adr_ieee) != 0) {
-		// feedback
-		show("[0x%04x] -> Device IEEE couldn't be read", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// read manufacturer name cluster
-	zcl_cluster_record_t wr;
-	if (znp_cmd_cluster_in_read(dev->adr_short, 0x0000, 0x0004, &wr) != 0) {
-		// feedback
-		show("[0x%04x] -> Device couldn't be read", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// check received type
-	if (wr.type != ZCL_CHARACTER_STRING) {
-		// feedback
-		show("[0x%04x] -> Device response type not ok", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// save the name
-	memcpy(dev->manufact_name, wr.data_arr, MIN(32, wr.data_arr_len));
-
-	// read device name cluster
-	if (znp_cmd_cluster_in_read(dev->adr_short, 0x0000, 0x0005, &wr) != 0) {
-		// feedback
-		show("[0x%04x] -> Device couldn't be read", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// check received type
-	if (wr.type != ZCL_CHARACTER_STRING) {
-		// feedback
-		show("[0x%04x] -> Device response type not ok", dev->adr_short);
-
-		// done, stop the task
-		goto end_task;
-	}
-
-	// save the name
-	memcpy(dev->device_name, wr.data_arr, MIN(32, wr.data_arr_len));
-
-	// print device info
-	uint32_t top = dev->adr_ieee >> 32;
-	uint32_t bot = dev->adr_ieee & 0xffffffff;
-	show("[0x%04x] -> IEEE Address: 0x%08x%08x", dev->adr_short, top, bot);
-	show("[0x%04x] -> Manufacturer name: %s", dev->adr_short,
-			dev->manufact_name);
-	show("[0x%04x] -> Device name: %s", dev->adr_short, dev->device_name);
-
-	// save the device
-	zigbee_device_save(dev);
-
-// stop the task
-	end_task: vTaskDelete(NULL);
-}
-
-void device_active_response(uint16_t adr) {
-	xTaskCreate(task_register_device, "dev-reg", 1024, (void*) &adr, 6, NULL);
-	vTaskDelay(10);
-}
-
-void rcpWaitPeriod(uint32_t period) {
-	uint32_t waittime = period;
-	uint32_t start = xTaskGetTickCount();
-	while (rpcWaitMqClientMsg(waittime) == 0) {
-		uint32_t passed_time = start - xTaskGetTickCount();
-		waittime -= passed_time;
-		start = xTaskGetTickCount();
-	}
-}
-
-void phyReset(void) {
-	OsalNvWriteFormat_t req;
-	req.Id = 0x0003;
-	req.Offset = 0x00;
-	req.Len = 0x01;
-	req.Value[0] = 0x03;
-	sysOsalNvWrite(&req);
-	ResetReqFormat_t rst;
-	rst.Type = 0x00;
-	sysResetReq(&rst);
-}
-
-// init coordinator
-// taken from https://sunmaysky.blogspot.com/2017/02/use-ztool-z-stack-30-znp-to-set-up.html
-int znp_init_coordinator(uint8_t enable_commissioning) {
-	OsalNvWriteFormat_t req;
-	setChannelFormat_t chn;
-	startCommissioningFormat_t strt;
-	ResetReqFormat_t rst;
-
-	// wait a second
-	vTaskDelay(1000);
-	show("1 ----------------------");
-
-	// soft reset
-	rst.Type = 0x01;
-	sysResetReq(&rst);
-
-	vTaskDelay(4000);
-	show("2 ----------------------");
-
-	// Write ZCD_NV_LOGICAL_TYPE to 0 which means coordinator
-	req.Id = 0x0087;
-	req.Offset = 0x00;
-	req.Len = 0x01;
-	req.Value[0] = 0x00;
-	sysOsalNvWrite(&req);
-
-	vTaskDelay(1000);
-	show("3 ----------------------");
-
-	// set primary channel to 13
-	chn.primaryChannel = 1;
-	chn.channel = CFG_CHANNEL_0x00002000;
-	appCfgSetChannel(&chn);
-
-	vTaskDelay(1000);
-	show("4 ----------------------");
-
-	// disable secondary channel
-	chn.primaryChannel = 0;
-	chn.channel = CFG_CHANNEL_NONE;
-	appCfgSetChannel(&chn);
-
-	vTaskDelay(1000);
-	show("5 ----------------------");
-
-	// start commissioning using network formation
-	strt.commissioningMode = CFG_COMM_MODE_NWK_FORMATION;
-	appCfgStartCommissioning(&strt);
-
-	vTaskDelay(10000);
-	show("6 ----------------------");
-
-	if (enable_commissioning) {
-		// get device info
-		utilGetDeviceInfo();
-
-		vTaskDelay(1000);
-		show("7 ----------------------");
-
-		// Write ZCD_NV_LOGICAL_TYPE to 0 which means coordinator
-		req.Id = 0x008F;
-		req.Offset = 0x00;
-		req.Len = 0x01;
-		req.Value[0] = 0x01;
-		sysOsalNvWrite(&req);
-
-		vTaskDelay(1000);
-		show("8 ----------------------");
-
-		// start commissioning using network steering
-		strt.commissioningMode = CFG_COMM_MODE_NWK_STEERING;
-		appCfgStartCommissioning(&strt);
-	} else {
-		// get device info
-		utilGetDeviceInfo();
-	}
-
-	//
-	return 0;
-}
-
-void register_clusters(uint16_t addr) {
-	vTaskDelay(20000);
-
-	// check registration
-	if (!znp_if_dev_exists(addr)) {
-		show("-> !!! Device not registered!");
-		return;
-	}
-
-	show("9 ----------------------");
-
-	// wait for device to be active
-	while (1) {
-		int ret = znp_cmd_dev_is_active(addr);
-		show("znp_if_dev_is_active %d", ret);
-		if (ret == 0)
-			break;
-		vTaskDelay(1000);
-	}
-	show("10 ----------------------");
-
-	// refresh device info
-	show("znp_if_dev_refresh_info %d", znp_cmd_dev_refresh_info(addr));
-	vTaskDelay(1000);
-	show("11 ----------------------");
-
-	// register device
-	show("znp_if_dev_register %d", znp_cmd_dev_register(addr));
-	vTaskDelay(1000);
-	show("12 ----------------------");
-
-	// read device name cluster
-	zcl_cluster_record_t wr;
-	show("znp_cmd_cluster_in_read %d",
-			znp_cmd_cluster_in_read(addr, 0, 4, &wr));
-	show("Type: %d", wr.type);
-	show("Str: %s", wr.data_arr);
-
-	// write thermostat to 19 degree
-	wr.type = ZCL_SIGNED_16BITS;
-	wr.data_i16 = 1900;
-	show("znp_cmd_cluster_in_write %d",
-			znp_cmd_cluster_in_write(addr, 0x0201, 0x0012, &wr));
-
-	// read thermostat value
-	show("znp_cmd_cluster_in_read %d",
-			znp_cmd_cluster_in_read(addr, 0x0201, 0x0012, &wr));
-	show("Type: %d", wr.type);
-	show("Data: %d", wr.data_i16);
-}
-
-uint8_t show(const char *fmt, ...) {
+static uint8_t show(const char *fmt, ...) {
 	struct AppMessage msg;
 	msg.ucMessageID = MID_VW_LOG;
 	memset(msg.content, 0, sizeof(msg.content));
@@ -466,16 +175,42 @@ void resetRtr() {
 }
 
 void scan() {
+	MgmtLqiReqFormat_t req;
+	req.DstAddr = 0;
+	req.StartIndex = 0;
+	uint8_t status;
+	while (1) {
+		nodeCount = 0;
+		zdoMgmtLqiReq(&req);
+		while (status != -1) {
+			status = rpcWaitMqClientMsg(1000);
+		}
+		status = 0;
+		uint8_t i;
+		for (i = 0; i < nodeCount; i++) {
+			char *devtype = (
+					nodeList[i].Type == DEVICETYPE_ROUTER ?
+							"ROUTER" : "END DEVICE");
+			if (nodeList[i].Type == DEVICETYPE_COORDINATOR) {
+				devtype = "COORDINATOR";
+			}
+			show("Node Address: 0x%04X   Type: %s\n", nodeList[i].NodeAddr,
+					devtype);
 
+			show("Children: %d\n", nodeList[i].ChildCount);
+			uint8_t cI;
+			for (cI = 0; cI < nodeList[i].ChildCount; cI++) {
+				uint8_t type = nodeList[i].childs[cI].Type;
+				show("\tAddress: 0x%04X   Type: %s\n",
+						nodeList[i].childs[cI].ChildAddr,
+						(type == DEVICETYPE_ROUTER ? "ROUTER" : "END DEVICE"));
+			}
+		}
+	}
 }
 
 void start() {
 	show("Starting");
-	/*
-	 ResetReqFormat_t resReq = { .Type = 1 };
-	 sysResetReq(&resReq);
-	 rpcWaitMqClientMsg(5000);
-	 */
 	uint8_t status = registerAf();
 	if (status != MT_RPC_SUCCESS) {
 		show("Register Af failed");
@@ -493,12 +228,6 @@ void start() {
 		status = -1;
 	}
 	show("ZigBee state machine running");
-	/*
-	 while (status != -1) {
-	 status = rpcWaitMqClientMsg(5000);
-	 }
-	 status = setNVStartup(0);
-	 */
 }
 
 void vAppTaskLoop() {
@@ -543,26 +272,6 @@ void vAppTask(void *pvParameters) {
 		vAppTaskLoop();
 }
 
-//init ZDO device state
-uint8_t gSrcEndPoint = 1;
-uint8_t gDstEndPoint = 1;
-
-typedef struct {
-	uint16_t ChildAddr;
-	uint8_t Type;
-
-} ChildNode_t;
-
-typedef struct {
-	uint16_t NodeAddr;
-	uint8_t Type;
-	uint8_t ChildCount;
-	ChildNode_t childs[MAX_CHILDREN];
-} Node_t;
-
-Node_t nodeList[MAX_NODE_LIST];
-uint8_t nodeCount = 0;
-
 /********************************************************************
  * START OF ZDO CALL BACK FUNCTIONS
  */
@@ -571,50 +280,40 @@ static uint8_t mtZdoStateChangeIndCb(uint8_t newDevState) {
 
 	switch (newDevState) {
 	case DEV_HOLD:
-		show("mtZdoStateChangeIndCb: Initialized - not started automatically");
+		show("Hold");
 		break;
 	case DEV_INIT:
-		show("mtZdoStateChangeIndCb: Initialized - not connected to anything");
+		show("Init");
 		break;
 	case DEV_NWK_DISC:
-		show("mtZdoStateChangeIndCb: Discovering PAN's to join");
 		show("Network Discovering");
 		break;
 	case DEV_NWK_JOINING:
-		show("mtZdoStateChangeIndCb: Joining a PAN");
 		show("Network Joining");
 		break;
 	case DEV_NWK_REJOIN:
 		show("Network Rejoining");
-		show("mtZdoStateChangeIndCb: ReJoining a PAN, only for end devices");
 		break;
 	case DEV_END_DEVICE_UNAUTH:
 		show("Network Authenticating");
-		show(
-				"mtZdoStateChangeIndCb: Joined but not yet authenticated by trust center");
 		break;
 	case DEV_END_DEVICE:
-		show("Network Joined");
-		show("mtZdoStateChangeIndCb: Started as device after authentication");
+		show("Network Joined as End Device");
 		break;
 	case DEV_ROUTER:
-		show("Device joined as Router");
+		show("Network Joined as Router");
 		break;
 	case DEV_COORD_STARTING:
 		show("Network Starting");
-		show("mtZdoStateChangeIndCb: Started as Zigbee Coordinator");
 		break;
 	case DEV_ZB_COORD:
 		show("Network Started");
-		show("mtZdoStateChangeIndCb: Started as Zigbee Coordinator");
 		break;
 	case DEV_NWK_ORPHAN:
 		show("Network Orphaned");
-		show(
-				"mtZdoStateChangeIndCb: Device has lost information about its parent");
 		break;
 	default:
-		show("mtZdoStateChangeIndCb: unknown state");
+		show("Unknown state");
 		break;
 	}
 
@@ -734,4 +433,56 @@ static int32_t registerAf(void) {
 
 	status = afRegister(&reg);
 	return status;
+}
+
+static uint8_t mtZdoSimpleDescRspCb(SimpleDescRspFormat_t *msg) {
+
+	if (msg->Status == MT_RPC_SUCCESS) {
+		show("\tEndpoint: 0x%02X\n", msg->Endpoint);
+		show("\tProfileID: 0x%04X\n", msg->ProfileID);
+		show("\tDeviceID: 0x%04X\n", msg->DeviceID);
+		show("\tDeviceVersion: 0x%02X\n", msg->DeviceVersion);
+		show("\tNumInClusters: %d\n", msg->NumInClusters);
+		uint32_t i;
+		for (i = 0; i < msg->NumInClusters; i++) {
+			show("\t\tInClusterList[%d]: 0x%04X\n", i, msg->InClusterList[i]);
+		}
+		show("\tNumOutClusters: %d\n", msg->NumOutClusters);
+		for (i = 0; i < msg->NumOutClusters; i++) {
+			show("\t\tOutClusterList[%d]: 0x%04X\n", i, msg->OutClusterList[i]);
+		}
+		show("\n");
+	} else {
+		show("SimpleDescRsp Status: FAIL 0x%02X\n", msg->Status);
+	}
+
+	return msg->Status;
+}
+static uint8_t mtZdoActiveEpRspCb(ActiveEpRspFormat_t *msg) {
+
+	SimpleDescReqFormat_t simReq;
+	if (msg->Status == MT_RPC_SUCCESS) {
+		simReq.DstAddr = msg->NwkAddr;
+		simReq.NwkAddrOfInterest = msg->NwkAddr;
+		show("Number of Endpoints: %d\n", msg->ActiveEPCount);
+		uint32_t i;
+		for (i = 0; i < msg->ActiveEPCount; i++) {
+			simReq.Endpoint = msg->ActiveEPList[i];
+			zdoSimpleDescReq(&simReq);
+		}
+	} else {
+		show("ActiveEpRsp Status: FAIL 0x%02X\n", msg->Status);
+	}
+
+	return msg->Status;
+}
+
+static uint8_t mtZdoEndDeviceAnnceIndCb(EndDeviceAnnceIndFormat_t *msg) {
+
+	ActiveEpReqFormat_t actReq;
+	actReq.DstAddr = msg->NwkAddr;
+	actReq.NwkAddrOfInterest = msg->NwkAddr;
+	show("\nNew device joined network.\nNwkAddr: 0x%04X\n", msg->NwkAddr);
+	zdoActiveEpReq(&actReq);
+	return 0;
 }
